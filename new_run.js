751e794a74d1474ae41c4b8419d46610543e298c
Crab1k <61062744+Crab1k@users.noreply.github.com>
2026-08-25 06:32:02 +0200

    Update new_run.js

--- new_run.js
+++ new_run.js
@@ -261,7 +261,7 @@ function playScreamer() {
 }
 
 function armScreamer(amount) {
-	if (type !== 'n') return;
+	if (type !== 'b') return;
 	if (!screamerEnabled || screamerArmed) return;
 	if (amount !== 1 && amount !== -1) return;
 	const onlyOneBound = (start === null) !== (end === null);
